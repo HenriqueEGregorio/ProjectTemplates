49c919fd405698f0c0b9d98c8ee375d2a1e82821
Henrique Gregorio <henriquee.gr@outlook.com>
2024-08-29 21:29:27 -0300

    Api Entity Template

---
+++ Templates/TamplateApiEntity/TamplateApiEntity.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace TamplateApiEntity.Domain.Entities;
+
+public class User : DefaultEntity
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
---
+++ Templates/TamplateApiEntity/TamplateApiEntity.Domain/Intefaces/IDefaultEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TamplateApiEntity.Domain.Intefaces;
+
+public interface IDefaultEntity
+{
+    Guid Id { get; set; }
+    int Sequence { get; set; }
+    DateTime CreatedAt { get; set; }
+    DateTime UpdateAt { get; set; }
+}
